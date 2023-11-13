4cab0594315efe8b55fe97b8332d00f4f70c570c
Ahsan <ahsanaz461@gmail.com>
2023-11-13 16:19:50 +0700

    feat: add create announcement

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,14 +9,14 @@ export default async function Admin() {
 
   return (
     <React.Fragment>
-      <div className="flex flex-col gap-2 mb-5">
+      <div className="mb-5 flex flex-col gap-2">
         <h1 className="text-xl font-bold">Data Pendaftar</h1>
-        <div className="grid grid-cols-2 xl:grid-cols-3 gap-2 md:gap-4">
+        <div className="grid grid-cols-2 gap-2 md:gap-4 xl:grid-cols-3">
           <Link
             href={"/admin/cyber-security"}
-            className="bg-gray-600 rounded-xl w-full p-4 border flex items-center gap-4 hover:bg-opacity-75 transition-all duration-300"
+            className="flex w-full items-center gap-4 rounded-xl border bg-gray-600 p-4 transition-all duration-300 hover:bg-opacity-75"
           >
-            <div className="p-2 bg-gray-200 text-primary rounded-xl">
+            <div className="text-primary rounded-xl bg-gray-200 p-2">
               <FaUser />
             </div>
             <div className="flex flex-col">
@@ -28,9 +28,9 @@ export default async function Admin() {
           </Link>
           <Link
             href={"/admin/cloud-computing"}
-            className="bg-gray-600 rounded-xl w-full p-4 border flex items-center gap-4 hover:bg-opacity-75 transition-all duration-300"
+            className="flex w-full items-center gap-4 rounded-xl border bg-gray-600 p-4 transition-all duration-300 hover:bg-opacity-75"
           >
-            <div className="p-2 bg-gray-200 text-primary rounded-xl">
+            <div className="text-primary rounded-xl bg-gray-200 p-2">
               <FaUser />
             </div>
             <div className="flex flex-col">
@@ -42,9 +42,9 @@ export default async function Admin() {
           </Link>
           <Link
             href={"/admin/web-design"}
-            className="bg-gray-600 rounded-xl w-full p-4 border flex items-center gap-4 hover:bg-opacity-75 transition-all duration-300"
+            className="flex w-full items-center gap-4 rounded-xl border bg-gray-600 p-4 transition-all duration-300 hover:bg-opacity-75"
           >
-            <div className="p-2 bg-gray-200 text-primary rounded-xl">
+            <div className="text-primary rounded-xl bg-gray-200 p-2">
               <FaUser />
             </div>
             <div className="flex flex-col">
@@ -56,9 +56,9 @@ export default async function Admin() {
           </Link>
           <Link
             href={"/admin/business-plan"}
-            className="bg-gray-600 rounded-xl w-full p-4 border flex items-center gap-4 hover:bg-opacity-75 transition-all duration-300"
+            className="flex w-full items-center gap-4 rounded-xl border bg-gray-600 p-4 transition-all duration-300 hover:bg-opacity-75"
           >
-            <div className="p-2 bg-gray-200 text-primary rounded-xl">
+            <div className="text-primary rounded-xl bg-gray-200 p-2">
               <FaUser />
             </div>
             <div className="flex flex-col">
@@ -70,9 +70,9 @@ export default async function Admin() {
           </Link>
           <Link
             href={"/admin/mobile-legends"}
-            className="bg-gray-600 rounded-xl w-full p-4 border flex items-center gap-4 hover:bg-opacity-75 transition-all duration-300"
+            className="flex w-full items-center gap-4 rounded-xl border bg-gray-600 p-4 transition-all duration-300 hover:bg-opacity-75"
           >
-            <div className="p-2 bg-gray-200 text-primary rounded-xl">
+            <div className="text-primary rounded-xl bg-gray-200 p-2">
               <FaUser />
             </div>
             <div className="flex flex-col">
@@ -84,9 +84,9 @@ export default async function Admin() {
           </Link>
           <Link
             href={"#"}
-            className="bg-gray-600 rounded-xl w-full p-4 border flex items-center gap-4 hover:bg-opacity-75 transition-all duration-300"
+            className="flex w-full items-center gap-4 rounded-xl border bg-gray-600 p-4 transition-all duration-300 hover:bg-opacity-75"
           >
-            <div className="p-2 bg-gray-200 text-primary rounded-xl">
+            <div className="text-primary rounded-xl bg-gray-200 p-2">
               <FaUser />
             </div>
             <div className="flex flex-col">
@@ -99,14 +99,8 @@ export default async function Admin() {
       <div className="flex flex-col gap-2">
         <div className="flex justify-between">
           <h1 className="text-xl font-bold">Pengumuman</h1>
-          <button
-            className="bg-emerald-500 text-white hover:bg-emerald-600 active:bg-emerald-700 font-bold uppercase text-sm px-3 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-          >
-            Add New
-          </button>
         </div>
-        <div className="w-full overflow-hidden rounded-lg shadow-xs">
+        <div className="shadow-xs w-full overflow-hidden rounded-lg">
           <div className="w-full overflow-x-auto">
             <AnnouncementTable announcements={announcements} />
           </div>
@@ -115,3 +109,5 @@ export default async function Admin() {
     </React.Fragment>
   );
 }
+
+export const revalidate = 0;
