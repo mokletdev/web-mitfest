cc904f62e12e89c009a16a0df835854f1758b772
Ahsan Awadullah Azizan <ahsanaz461@gmail.com>
2023-11-20 13:44:48 +0700

    Landing page and minor fixes (#17)
    * feat: add registrations page
    
    * docs: update how to run guide
    
    * fix chore: redesign login page and add color palette
    
    * feat: add register page
    
    * fix: add pointer-events none to labels
    
    * fix: change font weights
    
    * fix: revalidate view registrations admin page
    
    * feat: add landing page
    
    * fix: create prisma client first then export it
    
    * feat fix: add landing page's navbar and remove global h1 style
    
    * chore: separate landing page into several components
    
    * fix: increase navbar's backdrop blur
    
    * featL add background animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function RootPage() {
-  return <></>;
+import Link from "next/link";
+import React from "react";
+import Navbar from "./components/Parts/Navbar";
+import Beranda from "./components/Parts/Beranda";
+
+export default function Root() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <main className="w-screen bg-black relative">
+        <Beranda />
+      </main>
+    </React.Fragment>
+  );
 }
