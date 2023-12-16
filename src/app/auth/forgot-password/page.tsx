691e0067aeaa7f7cdd07dbbecd2f47aa8434a310
Kusindra Aji Rabbany <me@benspace.xyz>
2023-12-16 21:55:33 +0700

    fix: typo dikit ga ngaruh

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -72,7 +72,7 @@ export default function ForgotPassword() {
         </button>
         <div className="mb-6 flex flex-col justify-center gap-2 md:justify-start">
           <h6 className="mb-3 text-[28px] leading-[36px] text-white sm:text-[32px] sm:leading-[40px]">
-            Lupa Kata Sandhi
+            Lupa Kata Sandi
           </h6>
           <span className="text-neutral-500">
             Masukkan email Akun untuk melakukan reset kata sandi
