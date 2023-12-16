e14f75abbd0f48c8fc85429ece512c403d35d51c
Ahsan <ahsanaz461@gmail.com>
2023-12-16 08:34:38 +0700

    feat: FInish participant main dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,6 @@ import Image from "next/image";
 import { LinkButton } from "../components/Button";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import CalendarIcon from "@/app/components/Icons/Calendar";
-import ArrowIcon from "../components/Icons/Arrow";
-import { FormButton } from "../components/Button";
 import { getAllAnnounce } from "@/utils/queries/announcement.query";
 import { convertTimezone, stringifyDate, stringifyTime } from "@/utils/date";
 
@@ -12,7 +9,7 @@ export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
   const announcements = (await getAllAnnounce())
     .filter((announcement) => announcement.type === "all")
-    .reverse();
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
 
   return (
     <>
@@ -23,13 +20,6 @@ export default async function DashboardPage() {
         <div className="rounded-lg bg-black p-8">
           <div className="mb-7 flex w-full flex-col items-start justify-between gap-[22px] md:flex-row md:items-center md:gap-0">
             <h6 className="text-lg">Pengumuman</h6>
-            <FormButton className="flex items-center gap-[22px]" variant="red">
-              <div className="flex items-center gap-3">
-                <CalendarIcon />
-                {stringifyDate(new Date())}
-              </div>
-              <ArrowIcon className="rotate-180" />
-            </FormButton>
           </div>
           <div className="flex w-full flex-col gap-[42px]">
             {announcements.map((announcement, i) => (
