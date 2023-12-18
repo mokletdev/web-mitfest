889cf5394515f7665d4503565df0c5327bcaf90d
Muhammad Zuhair Zuhdi <joe@frostfire.id>
2023-12-18 15:36:14 +0700

    feat: Front end
    Co-authored-by: Naufal Nabil Ramadhan <naufalnrsmitter20@users.noreply.github.com>

--- src/app/dashboard/components/BuktiPembayaran.tsx
+++ src/app/dashboard/components/BuktiPembayaran.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+"use client";
 
-export default function FormTim() {
+import { FormButton, LinkButton } from '@/app/components/Button';
+import React from 'react'
+
+export default function BuktiPembayaran() {
   return (
     <>
-    <section className="m-0 pr-[24px] pl-[24px]">
-      <section className="xl:justify-beetween xl:flex w-full max-w-full xl:w-[747px] rounded-md bg-black p-[24px] lg:p-[32px] md:max-w-lg lg:max-w-xl xl:max-w-full">
-      <div className="flex mb-[62px] xl:invisible xl:absolute xl:top-0 xl:left-0">
+      <section className="m-0 pr-[24px] pl-[24px]">
+        <section className="xl:justify-beetween xl:flex w-full max-w-full xl:w-[747px] rounded-md bg-black p-[24px] lg:p-[32px] md:max-w-lg lg:max-w-xl xl:max-w-full">
+          <div className="flex mb-[62px] xl:invisible xl:absolute xl:top-0 xl:left-0">
             <div className="m-auto rounded-full bg-primary-500 px-5 py-3 text-[18px] text-white">
               1
             </div>
@@ -13,20 +16,20 @@ export default function FormTim() {
             <div className="m-auto rounded-full bg-primary-500 px-5 py-3 text-[18px] text-white">
               2
             </div>
-            <div className="w-full h-0.5 bg-gray-600 my-auto mx-2"></div>
-            <div className="m-auto rounded-full border-2 border-gray-600 bg-black px-5 py-3 text-[18px] text-gray-600">
+            <div className="w-full h-0.5 bg-primary-500 my-auto mx-2"></div>
+            <div className="m-auto rounded-full bg-primary-500 px-5 py-3 text-[18px] text-white">
               3
             </div>
-            <div className="w-full h-0.5 bg-gray-600 my-auto mx-2"></div>
-            <div className="m-auto rounded-full border-2 border-gray-600 bg-black px-5 py-3 text-[18px] text-gray-600">
+            <div className="w-full h-0.5 bg-primary-500 my-auto mx-2"></div>
+            <div className="m-auto rounded-full bg-primary-500 px-5 py-3 text-[18px] text-white">
               4
             </div>
             
-      </div>
+          </div>
 
         <div className="block invisible xl:visible absolute top-0 left-0 xl:relative">
           <div className="flex">
-            <div className="relative block w-[153px] max-w-full">
+            <div className="max-w-full relative block w-[153px]">
               <h2 className="text-right text-[18px] text-white">
                 Pilihan Bergabung
               </h2>
@@ -74,7 +77,7 @@ export default function FormTim() {
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M1 0V42" stroke="#656363" stroke-width="1.8" />
+                  <path d="M1 0V42" stroke="#B10F00" stroke-width="1.8" />
                 </svg>
               </div>
             </div>
@@ -89,7 +92,7 @@ export default function FormTim() {
               </p>
             </div>
             <div className="ml-[20px] block">
-              <div className="m-auto rounded-full border-2 border-gray-600 bg-black px-5 py-3 text-[18px] text-gray-600">
+              <div className="m-auto rounded-full bg-primary-500 px-5 py-3 text-[18px] text-white">
                 3
               </div>
               <div className="block h-[42px] w-[52px] py-2">
@@ -101,7 +104,7 @@ export default function FormTim() {
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M1 0V42" stroke="#656363" stroke-width="1.8" />
+                  <path d="M1 0V42" stroke="#B10F00" stroke-width="1.8" />
                 </svg>
               </div>
             </div>
@@ -114,7 +117,7 @@ export default function FormTim() {
               <p className="text-right text-[16px] text-gray-500">Screenshot</p>
             </div>
             <div className="ml-[20px] block">
-              <div className="m-auto rounded-full border-2 border-gray-600 bg-black px-5 py-3 text-[18px] text-gray-600">
+              <div className="m-auto rounded-full bg-primary-500 px-5 py-3 text-[18px] text-white">
                 4
               </div>
             </div>
@@ -122,58 +125,35 @@ export default function FormTim() {
         </div>
 
         <div className="xl:ml-[62px] xl:mr-[32px] block w-[398px]">
-          <p className="mb-[16px] text-[14px] text-gray-500">Step 2/4</p>
-          <h2 className="mb-[8px] text-[24px] text-white">Informasi Tim</h2>
+        <p className="mb-[16px] text-[14px] text-gray-500">Step 4/4</p>
+          <h2 className="mb-[8px] text-[24px] text-white">Bukti Pembayaran</h2>
           <p className="text-[16px] text-gray-500 mb-[28px]">
-          Mohon untuk mengisi dengan benar dan teliti.
+          Upload screenshot bukti pembayaran
           </p>
           <div className="block">
-            <input
-              className="mb-[14px] mr-[14px] h-[54px] w-full rounded-lg border-2 border-gray-500 bg-black px-[16px]"
-              type="text"
-              name="NamaAnggota"
-              id="NamaAnggota"
-              placeholder="Nama tim"
-            />
-            <input
-              className="mb-[14px] mr-[14px] h-[54px] w-full rounded-lg border-2 border-gray-500 bg-black px-[16px]"
-              type="text"
-              name="instansi"
-              id="instansi"
-              placeholder="Instansi"
-            />
-            <input
-              className="mb-[14px] mr-[14px] h-[54px] w-full rounded-lg border-2 border-gray-500 bg-black px-[16px]"
-              type="text"
-              name="NamaKetua"
-              id="NamaKetua"
-              placeholder="Nama Ketua"
-            />
-            <input
-              className="mb-[14px] mr-[14px] h-[54px] w-full rounded-lg border-2 border-gray-500 bg-black px-[16px]"
-              type="text"
-              name="No-WhatsApp"
-              id="No-WhatsApp"
-              placeholder="No. WhatsApp (ketua)"
-            />
-            <label className="mb-[20px] text-gray-400">
-                    Kartu Pelajar (Ketua)
+          <label className="mb-[20px] text-gray-400">
+                    Bukti pembayaran
                   </label>
                   <input
                     className="tb-[14px] mb-[14px] mr-[14px] block h-[54px] w-full rounded-lg border-2 border-gray-500 bg-black px-[16px] py-2.5 text-gray-500"
                     type="file"
-                    name="Foto-Anggota"
+                    name="bukti-pembayaran"
                     // id={`name-${index}`}
                     // value={item.name}
                     // onChange={(e) =>
                     //   handlePhotoChange(index, e.target.files[0] ?? null)
                     // }
-                    placeholder="Foto Anggota"
+                    placeholder="Bukti Pembayaran"
                   />
           </div>
+                <div className="mt-[48px] flex justify-end">
+                    <LinkButton className="mr-[16px] rounded-full border-2 border-gray-600 bg-black px-7 py-3 text-[16px] text-white" href={''}>Sebelumnya</LinkButton>
+                    <FormButton className="mr-[16px] rounded-full border-2 border-primary-400 bg-primary-500 hover:bg-primary-400 px-7 py-3 text-[16px] text-white">Kirim</FormButton>
+
+                </div>
         </div>
-      </section>
-      </section>
+        </section>
+        </section>
     </>
-  );
+  )
 }
---
+++ src/app/dashboard/components/Notifikasi.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react'
+
+export default function Notifikasi() {
+  return (
+    <>
+        <section className="m-0 pr-[24px] pl-[24px]">
+        <section className="rounded-md bg-black p-[24px] lg:p-[32px] w-[1127px] sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-full">
+            <h1 className="text-white mb-[18px]">Terimakasih Telah Mendaftar!</h1>
+            <p className="text-gray-500">Berkas pendaftaran Anda telah diterima. Kami akan memberitahu Anda melalui email segera setelah proses peninjauan selesai. Mohon periksa kotak masuk Gmail Anda untuk pembaruan status. Terima kasih!</p>
+        </section>
+        </section>
+    </>
+  )
+}
